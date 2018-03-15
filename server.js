b2443d8fc5d6e6d2ac3285b372498f868503576c
mjsimon91 <mjsimon91@gmail.com>
2018-03-14 22:47:57 -0400

    Added the server.js file on Port 3000

---
+++ server.js
@@ -0,0 +1,28 @@
+//Setting the dependencies 
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//Set the Port
+var PORT = process.env.PORT || 3000;
+
+//serve the static content from the public folder
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//parse the JSON application
+app.use(bodyParser.json());
+
+//If we decide to use handlebars...uncomment the next few lines
+//var exphbs = require('express-handlebars');
+
+//The handlebars files are .handlebars
+// app.engine("handlebars", exphbs({defaultLayout: 'main'}));
+// app.set("view engine", "handlebars");
+
+app.listen(PORT, function(){
+    console.log(`The application is now listening on Port ${PORT}`)
+})
