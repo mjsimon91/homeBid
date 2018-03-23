9e865946cd217a3bd0adb20f511ae60da031b90f
michael bychkowski <mbychkowski@gmail.com>
2018-03-23 18:17:23 -0400

    create image model, create controller folder for iamges

---
+++ models/images.js
@@ -0,0 +1,35 @@
+module.exports = function(sequelize, DataTypes) {
+  var Images = sequelize.define('Images', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+
+    imgUrl: {
+      type: DataTypes.STRING,
+      isUrl: true
+    },
+
+    isHomeImg: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+
+    isProfileImg: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+
+    // tags
+
+  });
+
+  //Associate Images to Members & Homes
+  Images.associate = function(models) {
+    Images.belongsTo(models.Members);
+    Images.belongsTo(models.Homes);
+  };
+
+  return Images;
+
+x}
